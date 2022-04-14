c3289b3c6eef8b27f47f041f262428c90e63b769
Artur <sarnackiartur@gmail.com>
2022-04-14 13:27:32 +0200

    modify app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./components/HomeScreen";
+import  HomeScreen  from "./components/HomeScreen";
 import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./components/test";
+import Player from "./components/Player";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <HomeScreen />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="homescreen" element={<HomeScreen />} />
+          <Route path="/sign" element={<SignIn />} />
+          <Route path="/player" element={<Player />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
